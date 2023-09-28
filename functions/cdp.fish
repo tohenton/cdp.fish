0299e330163cfba7f93a838acdfec5c4bb959f35
tohenton <tohenton@gmail.com>
2023-09-28 21:15:18 +0900

    feat: Add new common echo error function

--- functions/cdp.fish
+++ functions/cdp.fish
@@ -1,22 +1,26 @@
 function cdp --description "cd with project name under repo"
     if [ (count $argv) -eq 0 ]
-        echo "cdp: missing argument" > /dev/stderr
-        echo "Usage: cdp REPO_PROJECT_NAME" > /dev/stderr
+        __echo_error "cdp: missing argument"
+        __echo_error "Usage: cdp REPO_PROJECT_NAME"
         return 1
     end
 
     # repo command is not installed
     if not type -q repo
-        echo "fatal: no repo command installed" > /dev/stderr
+        __echo_error "fatal: no repo command installed"
         return 1
     end
 
     # executed not in repo repository
     if repo help | grep -q "repo is not yet installed"
-        echo "fatal: not a repo repository: .repo" > /dev/stderr
+        __echo_error "fatal: not a repo repository: .repo"
         return 1
     end
 
     builtin cd (repo manifest | grep $argv[1] | grep -o 'path="[^"]*' | sed 's/path="//')
     return 0
+end
+
+function __echo_error
+    echo $argv[1] > /dev/stderr
 end
