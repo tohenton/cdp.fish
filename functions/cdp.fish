76207ea367bf2dcc18d2b80ac7ab3506132a73ef
tohenton <tohenton@gmail.com>
2023-09-28 21:19:31 +0900

    feat: Add rpath to show a project path
    And use rpath as core of cdp

--- functions/cdp.fish
+++ functions/cdp.fish
@@ -1,11 +1,9 @@
-function cdp --description "cd with project name under repo"
+function rpath --description "Show a project path under repo"
+    set -l argv1 (status function)
     if test (count $argv) -eq 0
-        if not __is_repo_initialized
-            __echo_error "fatal: not a repo tree"
-            return 1
-        end
-        cd (__find_repo_root)
-        return 0
+        __echo_error "$argv1: missing argument"
+        __echo_error "Usage: $argv1 REPO_PROJECT_NAME"
+        return 1
     end
 
     # repo command is not installed
@@ -20,8 +18,32 @@ function cdp --description "cd with project name under repo"
         return 1
     end
 
-    builtin cd (repo manifest | grep $argv[1] | grep -o 'path="[^"]*' | sed 's/path="//')
+    set -l safe_arg (string escape -- $argv[1])
+    echo (repo manifest | grep $safe_arg | string match -r 'path="[^"]*' | string replace 'path="' '')
     return 0
+end
+
+function cdp --description "cd with project name under repo"
+    if test (count $argv) -eq 0
+        if not __is_repo_initialized
+            __echo_error "fatal: not a repo tree"
+            return 1
+        end
+        cd (__find_repo_root)
+        return 0
+    end
+
+    set -l repository_path (rpath $argv[1])
+    if test $status -ne 0
+        return 1
+    end
+
+    if test -z $repository_path
+        __echo_error "$argv[1] is missing"
+        return 1
+    end
+
+    cd $repository_path
 end
 
 function __is_repo_initialized
